25738fe8acd0960cdb89c221150b3a42a5dfe240
Nick Surface <nick.trapp@gmx.de>
2024-04-11 07:40:50 +0200

    Added comments to index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+// Import statements
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 
+
+// Mongoose setup
 const blogSchema = new mongoose.Schema({
   title: String,
   author: String,
@@ -15,9 +18,12 @@ const Blog = mongoose.model('Blog', blogSchema)
 const mongoUrl = 'mongodb://localhost/bloglist'
 mongoose.connect(mongoUrl)
 
+// Middleware
 app.use(cors())
 app.use(express.json())
 
+
+// API Routes
 app.get('/api/blogs', (request, response) => {
   Blog
     .find({})
@@ -36,6 +42,7 @@ app.post('/api/blogs', (request, response) => {
     })
 })
 
+// Running the server
 const PORT = 3003
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
