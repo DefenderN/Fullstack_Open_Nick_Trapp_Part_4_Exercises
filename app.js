494924077d5d0827a63d66f164e275c38fd52d36
Nick Surface <nick.trapp@gmx.de>
2024-04-16 21:08:29 +0200

    Refactored the app to use the newly created blogsRouter in controllers(blogs.js) to handle requests for the blog DB. Therefore removed exisiting hardcoded routes in the app.js file. Tested the new functions with the integrated REST extension of vscode

--- app.js
+++ app.js
@@ -6,9 +6,13 @@ const mongoose = require('mongoose')
 const logger = require('./utils/logger') // Import centralized logging module
 const config = require('./utils/config') // Import centralized config module, e.g. handling environment variables
 const middleware = require('./utils/middleware') // Import custom middleware
-const Blog = require('./models/blog') // Import Blog object to use for MongoDB
+const blogsRouter = require('./controllers/blogs') // Import Route controller for blogs in the DB
 
+// Settings
+mongoose.set('strictQuery', false)
 
+// Connect to MongoDB
+logger.info('connecting to', config.MONGODB_URI)
 mongoose.connect(config.MONGODB_URI)
   .then(result => {
     logger.info('Connected to MongoDB')
@@ -22,25 +26,8 @@ app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
 
-// API Routes
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-    .catch((error) => logger.error("Error occurred: ", error))
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-    .catch((error) => logger.error("Error occurred: ", error))
-})
+// Routers
+app.use('/api/blogs', blogsRouter)
 
 // Middleware AFTER Routes
 app.use(middleware.unknownEndpoint)
---
+++ controllers/blogs.js
@@ -0,0 +1,85 @@
+// This file is a separate piece of middleware
+// which specifies the details of the routes for Blog objects.
+// To do so, this file defines and exports a notesRouter
+// which is to be used in the app.js file (Where its relative
+// path/ route is defined.)
+// The route pathways defined here are RELATIVE pathways
+// to the pathway which is defined when this router is 
+// being used in the app.js file
+
+// Import statements
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog') // Import Blog object to use for MongoDB
+
+// ROUTES ------
+
+// Get all blogs
+blogsRouter.get('/', (request, response) => {
+    Blog.find({})
+        .then(blogs => {
+            response.json(blogs)
+        })
+})
+
+// Get an existing blog
+blogsRouter.get('/:id', (request, response, next) => {
+    Blog.findById(request.params.id)
+        .then(blog => {
+            if (blog) {
+                response.json(blog)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error)) // Pass any errors down the line
+})
+
+// Add a new blog
+blogsRouter.post('/', (request, response, next) => {
+
+    // Create new Blog object
+    const blog = new Blog({
+        title: request.body.title,
+        author: request.body.author,
+        url: request.body.url,
+        likes: request.body.likes
+    })
+
+    // Save new blog object to DB
+    blog.save()
+        .then(savedBlog => {
+            response.json(savedBlog)
+        })
+        .catch(error => next(error))
+})
+
+// DELETE a blog
+blogsRouter.delete('/:id', (request, response, next) => {
+    Blog.findByIdAndDelete(request.params.id)
+      .then(() => {
+        response.status(204).end()
+      })
+      .catch(error => next(error))
+})
+
+// Update a blog
+blogsRouter.put('/:id', (request, response, next) => {
+    const body = request.body
+  
+    // Create blog object to replace existing one
+    const blog = {
+        title: request.body.title,
+        author: request.body.author,
+        url: request.body.url,
+        likes: request.body.likes
+    }
+    // Update the DB with the new Blog object
+    Blog.findByIdAndUpdate(request.params.id, blog, { new: true, runValidators: true })
+      .then(updatedBlog => {
+        response.json(updatedBlog)
+      })
+      .catch(error => next(error))
+  })
+
+// Export statements
+module.exports = blogsRouter
