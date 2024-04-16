802e7c15b851dab47787528b0107566085907f1d
Nick Surface <nick.trapp@gmx.de>
2024-04-16 19:17:44 +0200

    Separated the index.js files content into two files: index.js and app.js.
    Index.js handles and starts the server while app.js contains the actual express app running on that "server"

--- index.js
+++ index.js
@@ -1,58 +1,7 @@
-// Import statements
-// require('dotenv').config() // import dotenv library to handle environment variables
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const logger = require('./utils/logger') // Import centralized logging module
+const app = require('./app') // the actual Express application
 const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-
-// Mongoose setup
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-// Create Blog object to handle DB requests
-const Blog = mongoose.model('Blog', blogSchema)
-
-mongoose.connect(config.MONGODB_URI)
-  .then(result => {
-    logger.info('Connected to MongoDB')
-  })
-  .catch(error => {
-    logger.error('Error connecting to MongoDB:', error.message)
-  })
-
-// Middleware
-app.use(cors())
-app.use(express.json())
-
-
-// API Routes
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-    .catch((error) => logger.error("Error occurred: ", error))
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-    .catch((error) => logger.error("Error occurred: ", error))
-})
-
-// Running the server
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
 })
