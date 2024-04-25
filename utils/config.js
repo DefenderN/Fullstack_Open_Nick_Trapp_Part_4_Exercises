008d5a6841867c49a4c1d53b9802074cc4bbd10a
Nick Surface <nick.trapp@gmx.de>
2024-04-25 15:39:00 +0200

    Modified scripts in the package.json file to consider the proper environment variables which different in development, testing and production stage.
    Problem: This does not work on Windows. For that we additionally install the cross-env package with:
    npm install --save-dev cross-env
    
    and
    
    npm install cross-env
    
    We can then achieve cross-platform compatibility by using the cross-env library in our npm scripts defined in package.json:
    
    Next we changed the config.js file (which is taking over the work of the node-config package that is used for larger applications) to pass different env variables, depending on testing mode or not.
    For this, we also have to add a TEST_MONGODB_URI variable in the .env file. (For the time being its the same as the other one)

--- utils/config.js
+++ utils/config.js
@@ -5,7 +5,9 @@ require('dotenv').config() // import dotenv library to handle environment variab
 
 // Environment variables
 const PORT = process.env.PORT
-const MONGODB_URI = process.env.MONGODB_URI
+const MONGODB_URI = process.env.NODE_ENV === 'test' 
+  ? process.env.TEST_MONGODB_URI
+  : process.env.MONGODB_URI
 
 // Export statement
 module.exports = {
