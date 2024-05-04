e8880836e2f8b91d3a4adf3ba45b78acaee7f938
DefenderN <57073766+DefenderN@users.noreply.github.com>
2024-05-04 16:42:41 +0200

    Finished Excercise 4.8: Blog List Tests, Step 1:
    -Reinstalled node_modules
    - Added beforeEach test functionality
    - Advanced test to check for a valid number of blogs in the blog DB

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,28 +1,47 @@
 // Import statements
 const {test, describe, beforeEach, after} = require("node:test")
+const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const Blog = require('../models/blog'); 
 
 // Assign the api variable the superagent, by passing the app object to the supertest function.
 const api = supertest(app)
 
+// Define test suite
 describe("API tests",() => {
+    // Clean up and initialize the database before each test
+    beforeEach(async () => {
+        await Blog.deleteMany({});
+        console.log("DB has been deleted")
+        // Create an array of dummy blog entries
+        const blogEntries = [
+            { title: "Blog 1", author: "Author 1", url: "http://example.com/1", likes: 10 },
+            { title: "Blog 2", author: "Author 2", url: "http://example.com/2", likes: 20 },
+            { title: "Blog 3", author: "Author 3", url: "http://example.com/3", likes: 30 },
+            { title: "Blog 4", author: "Author 4", url: "http://example.com/4", likes: 40 },
+            { title: "Blog 5", author: "Author 5", url: "http://example.com/5", likes: 50 },
+            { title: "Blog 6", author: "Author 6", url: "http://example.com/6", likes: 60 },
+        ];
 
-    /**
-     * Use the SuperTest library for writing a test that makes an HTTP GET request 
-     * to the /api/blogs URL. Verify that the blog list application returns the correct
-     * amount of blog posts in the JSON format. 
-     * 
-     * Once the test is finished, refactor the route handler to use the async/await syntax 
-     * instead of promises.
-     */
-    test('blogs are returned as json', async () => {
+    
+
+        // Insert the dummy blogs into the database
+        await Blog.insertMany(blogEntries);
+        console.log("Many blogs were inserted")
+    });
+
+
+    // Test to verify the correct amount of blogs returned in JSON format
+    test('there are exactly 6 blogs', async () => {
         const response = await api
             .get('/api/blogs')
             .expect(200)
             .expect('Content-Type', /application\/json/)
-    })
+        
+        assert.strictEqual(response.body.length, 6)
+    });
 
     // Close DB connections after testing
     after(async () => {
