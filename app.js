d7f721a42fc77254526a57c4048fe0542bcec810
DefenderN <57073766+DefenderN@users.noreply.github.com>
2024-05-21 14:11:57 +0200

    .

--- app.js
+++ app.js
@@ -25,11 +25,10 @@ mongoose.connect(config.MONGODB_URI)
 // Middleware BEFORE Routes
 app.use(cors())
 app.use(express.json())
-app.use(middleware.tokenExtractor)
 app.use(middleware.requestLogger)
 
 // Routers
-app.use('/api/blogs', blogsRouter)
+app.use('/api/blogs', middleware.tokenExtractor, blogsRouter)
 app.use('/api/users',usersRouter)
 app.use('/api/login', loginRouter)
 
